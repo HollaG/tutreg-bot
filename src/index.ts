14e65dfbcd69ec7856f4c0d690adeda96926db78
marcu <marcussoh38@gmail.com>
2023-07-04 15:34:41 +0800

    debug: console.log swap

--- src/index.ts
+++ src/index.ts
@@ -126,6 +126,13 @@ import {
             }
 
             // get the creator's class
+            console.log([
+                process.env.AY,
+                process.env.SEM,
+                swap.moduleCode,
+                swap.lessonType,
+                swap.classNo,
+            ]);
             const [creatorClasses]: [ModuleWithClassDB[], db.FieldPacket[]] =
                 await conn.query(
                     `SELECT * FROM modulelist LEFT JOIN classlist ON modulelist.moduleCode = classlist.moduleCode WHERE ay = ? AND semester = ? AND classlist.moduleCode = ? AND classlist.lessonType = ? AND classlist.classNo = ?`,
